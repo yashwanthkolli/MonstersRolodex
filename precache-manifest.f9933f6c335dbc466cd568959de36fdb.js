a7d6e6e50c7f7b2a415e0bbd574847cac46e5385
unknown <yashwanthkolli3271gmail.com>
2020-06-18 18:33:05 +0530

    Updates

---
+++ precache-manifest.f9933f6c335dbc466cd568959de36fdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81434956de3c2ec06c68dd3f08a2da5d",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "c26473ed0efdebf7c010",
+    "url": "/MonstersRolodex/static/css/main.ceea2f08.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/MonstersRolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26473ed0efdebf7c010",
+    "url": "/MonstersRolodex/static/js/main.825706bf.chunk.js"
+  },
+  {
+    "revision": "622d3e149693fc3a29ba",
+    "url": "/MonstersRolodex/static/js/runtime-main.07d671a0.js"
+  }
+]);
