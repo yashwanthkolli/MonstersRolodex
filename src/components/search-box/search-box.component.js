633b4385fa623306f9321d6199e25730f553f136
Yashwanth Kolli <yashwanthkolli3271gmail.com>
2020-11-29 15:37:48 +0530

    Final Changes

--- src/components/search-box/search-box.component.js
+++ src/components/search-box/search-box.component.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 import './search-box.styles.css'
 
-export const SearchBox = ({placeholder, handleChange}) => (
-    <input className='search' type="search" placeholder={placeholder} onChange={handleChange} />
+export const SearchBox = (props) => (
+    <input className='search' type="search" placeholder={props.placeholder} onChange={props.handleChange} />
 )
